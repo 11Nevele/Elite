e382af8b37deb625c7b0f79a40e8bb13cde29133
Nickwsy <wsynick@163.com>
2025-04-09 10:06:06 -0400

    Add borderless

--- src/game/Audio.java
+++ src/game/Audio.java
@@ -9,8 +9,7 @@ import javafx.scene.media.AudioClip;
 public class Audio 
 {
     public static javafx.scene.media.AudioClip explosionAsteroid;
-    public static AudioClip explosionShip;
-    public static AudioClip lazer;
-    public static javafx.scene.media.AudioClip ambient = 
-    new javafx.scene.media.AudioClip(new File(getCodeBase() + "\\Sound\\Ambient.wav").toURI().toString()); 
+    public static javafx.scene.media.AudioClip explosionShip;
+    public static javafx.scene.media.AudioClip lazer;
+    public static javafx.scene.media.AudioClip ambient;
 }
